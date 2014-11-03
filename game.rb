987f2a9b902044b6965a03af1df33100a7bb4ecd
Guilherme Freire Hack <gfreirehack@gmail.com>
2014-11-03 20:30:56 -0200

    read level

--- game.rb
+++ game.rb
@@ -1,3 +1,27 @@
+require 'highline/import'
+require 'highline/system_extensions'
+include HighLine::SystemExtensions
 require './level'
+require './player'
+
 @level = Level.new
-@level.read
+@end = false
+#w = get_character
+#puts w
+
+choose do |menu|
+   menu.prompt = "Choose your destiny:"
+   
+   Dir.glob("db/*.txt").map do |name|
+      menu.choice(%Q$:#{name}$) { @level.read(%Q$#{name}$) }
+   end
+end
+
+@level.draw
+
+until @end or @level.finished?
+   menu.choice(:up)    { }
+   menu.choice(:down)  { }
+   menu.choice(:left)  { }
+   menu.choice(:right) { }
+end
--- level.rb
+++ level.rb
@@ -1,14 +1,35 @@
 class Level
- @level
+ attr_accessor :layout, :player
 
  def initialize
+    @layout = Array.new
  end
 
  def read(name)
-    File.open("db/#{name}", "r") do |file|
+    @map = Array.new
+
+    File.open("#{name}", "r") do |file|
        file.map do |line|
-          @level = line.chomp.split("").inspect
+          @map << line.chomp
        end
+    end
+    
+    @map.each do |line|
+       @layout << line.split("")
+    end
+    #@player = Player.new
+ end
+
+ def finished?
+    return false
+ end
+
+ def draw
+    @layout.each do |line|
+       line.each do |ele|
+          print ele
+       end
+       print "\n"
     end
  end
 end
